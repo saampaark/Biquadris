f76e0ab0dbd811319475145feb1e86661d8c6702
Wenzhao Pan <140751736+wenzhaopan@users.noreply.github.com>
2025-01-20 17:44:43 -0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namesapce std;
+int main(){
+  cout<<"hello world"<<endl;
+  cout<<"this is temporary code, please refer to the readme if you want to see the real code"<<endl;
+}
